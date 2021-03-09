9d4eacf56232c745982ed4cd360e989b4718b445
alinavalovenko <alina.valovenko@gmail.com>
2021-03-09 20:23:06 +0200

    store registration added

--- src/Service/Maatoo/MtoConnector.php
+++ src/Service/Maatoo/MtoConnector.php
@@ -84,7 +84,7 @@ class MtoConnector
      *
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    private function getResponseData($endpointConfig): ?array
+    private function getResponseData($endpointConfig, $args = []): ?array
     {
         if (empty($endpointConfig)) {
             return [];
@@ -92,22 +92,16 @@ class MtoConnector
         try {
             $response = $this->client->request(
                 $endpointConfig->method,
-                $endpointConfig->route
+                $endpointConfig->route,
+                ['form_params' => $args]
             );
             $responseData = (array)json_decode($response->getBody()->getContents());
         } catch (Exception $exception) {
-            $responseData = null;
+            //TODO Add to log
+            return null;
         }
 
         return $responseData;
-    }
-
-    /**
-     * Check Configuration.
-     */
-    public function checkConfiguration()
-    {
-        //TODO check configuration and API endpoints
     }
 
     /**
@@ -117,7 +111,15 @@ class MtoConnector
      */
     public function registerStore(MtoStore $store)
     {
-        //TODO Register New Store
+        $formData = $store->toArray();
+        $endpoint = static::getApiEndPoint('store')->create ?? null;
+        $response = $this->getResponseData($endpoint, $formData);
+
+        if (!empty($response['store'])) {
+            return $store->setId($response['store']->id ?? null);
+        }
+
+        return false;
     }
 
     /**

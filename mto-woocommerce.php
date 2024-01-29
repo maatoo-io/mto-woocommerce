d6d89b6dca3c16029f1683d44fb79dbdfab90861
Simon Balz <simon@balz.me>
2024-01-29 08:11:23 +0100

    Release 1.9.6

--- mto-woocommerce.php
+++ mto-woocommerce.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: maatoo for WooCommerce
  * Description: Connect your online shop to drive more revenue with intelligent automations, e.g. abanoned cart reminders and more.
- * Version:     1.9.5
+ * Version:     1.9.6
  * Author: maatoo.io
  * Author URI: https://maatoo.io
  * License: GPL-3.0+
@@ -49,7 +49,7 @@ if (file_exists($composer_path)) {
 }
 
 if (!defined('MTO_PLUGIN_VERSION')) {
-    define('MTO_PLUGIN_VERSION', '1.9.5');
+    define('MTO_PLUGIN_VERSION', '1.9.6');
 }
 
 if (!defined('MTO_PLUGIN_SLUG')) {
